c9d397abe24008c80ab86bb4e23f869d33040b38
SPH <molchanoff.stephan@gmail.com>
2021-01-03 20:08:25 +1000

    Files Added

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private float _speed;
+
+    private Slider _healthBar;
+
+    private float GetNormalizedHealth => 
+        _player.Health / (float)_player.MaxHealth;
+
+    private void Start()
+    {
+        _healthBar = GetComponent<Slider>();
+        _healthBar.value = GetNormalizedHealth;
+    }
+    private void FixedUpdate()
+    {
+        if (GetNormalizedHealth != _healthBar.value)
+        {
+            if (Mathf.Abs(GetNormalizedHealth - _healthBar.value) <= 0.01f)
+                _healthBar.value = GetNormalizedHealth;
+            else
+                _healthBar.value = Mathf.Lerp(_healthBar.value, GetNormalizedHealth, _speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _health;
+
+    public int Health => _health;
+    public int MaxHealth => _maxHealth;
+
+    private void Start()
+    {
+        if (_health < 0 || _health > _maxHealth)
+            throw new ArgumentException(nameof(_health));
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        if (damage < 0)
+            throw new ArgumentException(nameof(damage));
+
+        _health -= damage > _health ? _health : damage;
+    }
+    public void ApplyHeal(int heal)
+    {
+        if (heal < 0)
+            throw new ArgumentException(nameof(heal));
+
+        _health += heal + _health > _maxHealth ? _maxHealth - _health : heal;
+    }
+}
